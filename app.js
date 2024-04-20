44aaac5bc72d1bdb9695bf086519dc72e2fe4304
sai shravan <saishravan384@gmail.com>
2024-04-20 09:31:01 +0530

    Add performance & security middleware

--- app.js
+++ app.js
@@ -4,12 +4,23 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const mongoose = require('mongoose')
+const compression = require('compression')
+const helmet = require('helmet')
+const RateLimit = require('express-rate-limit')
+require('dotenv').config()
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 const catalogRouter = require('./routes/catalog')
 
 const app = express()
+
+// set up mongoose connection
+mongoose.set('strictQuery', false)
+main().catch((err) => console.log(err))
+async function main() {
+  await mongoose.connect(process.env.MONGODB_URI)
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -19,7 +30,24 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(compression()) // Compress all routes
 app.use(express.static(path.join(__dirname, 'public')))
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      'script-src': ["'self'", 'code.jquery.com', 'cdn.jsdelivr.net'],
+    },
+  })
+)
+// Set up rate limiter: maximum of twenty requests per minute
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+})
+// Apply rate limiter to all requests
+app.use(limiter)
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
@@ -40,12 +68,5 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500)
   res.render('error')
 })
-
-mongoose.set('strictQuery', false)
-// Wait for database to connect, logging an error if there is a problem
-main().catch((err) => console.log(err))
-async function main() {
-  await mongoose.connect(process.env.MONGODB_URI)
-}
 
 module.exports = app
